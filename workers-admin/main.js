e993aee82554efdc796478ea6e6466b651b11ea7
IsacHolstad <wisac22@gmail.com>
2022-09-13 15:57:01 +0200

    debugging

--- workers-admin/main.js
+++ workers-admin/main.js
@@ -6,3 +6,32 @@ import './style.css'
 // use map()
 
 // use the destruct syntax
+// create api url
+const API_url = "https://dummy.restapiexample.com/api/v1/employees";
+const container = document.querySelector(".container");
+
+function getEmployees (){
+    fetch(API_url)
+        .then(data =>{
+            return data.json();
+    })
+        .then(({data:employeesData}) => {
+            console.log("employees data:", employeesData);
+            const newEmployeeData = employeesData.map(({employee_name, employee_age}) =>{
+                return  {employee_name, employee_age}
+            }).join('')
+            console.log("Employees name and age:", newEmployeeData);
+            container.innerHTML = newEmployeeData;
+        })
+}
+
+ getEmployees();
+
+
+
+
+
+
+
+
+
